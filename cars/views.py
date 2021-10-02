7d461a60eb53e3166f74e29139aa9497ad400d90
Ali <muhammadalised@gmail.com>
2021-10-02 22:38:30 +0500

    Added and setup car details page

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Car
 
 def cars(request):
+    cars = Car.objects.order_by("-created_date")
     context = {
         'page': 'cars',
+        'cars': cars
     }
     return render(request, 'cars/cars.html', context=context)
+
+def car_details(request, id):
+    car = get_object_or_404(Car, pk=id)
+    print(type(car.features))
+    context = {
+        'car': car,
+    }
+    return render(request, 'cars/car-details.html', context=context)
