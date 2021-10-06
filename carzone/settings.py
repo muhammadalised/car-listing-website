3081abbd7074b565ed26959f0a65084506c150c1
Ali <muhammadalised@gmail.com>
2021-10-06 19:46:38 +0500

    Added a line to settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -200,3 +201,5 @@ LOGGING = {
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+
+django_heroku.settings(config=locals(), staticfiles=False,logging=False)
