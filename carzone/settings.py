6b2183a4d4ee3f7fbc9fb3d5e4b99ab073653c9b
Ali <muhammadalised@gmail.com>
2021-10-06 19:10:07 +0500

    changes in settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -137,8 +137,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
