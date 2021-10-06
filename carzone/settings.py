2b0b8e8bbe512559db2c25d45ca53c376d7012fb
Ali <muhammadalised@gmail.com>
2021-10-06 16:45:59 +0500

    changed STATIC_ROOT to staticfiles

--- carzone/settings.py
+++ carzone/settings.py
@@ -137,7 +137,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
